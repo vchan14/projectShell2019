c20e420d252c165a810acdafd524e8d158ffd327
Visal <visal@Visals-MBP-712.T-mobile.com>
2019-10-30 23:04:54 -0700

    hello

--- api/start.js
+++ api/start.js
@@ -4,5 +4,5 @@ require('@babel/register')({
   presets: ['@babel/preset-env'],
 });
 
-// Import the rest of our application.
+// Import the rest of our application. :)
 module.exports = require('./server.js');
